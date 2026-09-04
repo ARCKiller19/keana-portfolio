9d5cecd6c2170490c47c4ffa45c6150c2a559141
kna <patriciakeanaroma.com@gmail.com>
2026-09-05 00:28:14 +0800

    Keep nav section clicks out of URL

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
+import { handleSectionNavigation } from '../utils/sectionNavigation.js'
+
 function Navbar() {
-    return (
-      <header className="nav" id="top">
-        <a className="nav-mark" href="#top">
-          KEANA
-        </a>
-  
-        <nav className="nav-links" aria-label="Primary">
-          <a href="#work">Work</a>
-          <a href="#motion">Motion</a>
-          <a href="#about">About</a>
-          <a href="#playground">Playground</a>
-          <a href="#contact">Contact</a>
-        </nav>
-  
-        <span className="nav-meta">Davao City, PH</span>
-      </header>
-    )
-  }
-  
-  export default Navbar
+  return (
+    <header className="nav" id="top">
+      <a className="nav-mark" href="#top" onClick={handleSectionNavigation}>
+        KEANA
+      </a>
+
+      <nav className="nav-links" aria-label="Primary">
+        <a href="#work" onClick={handleSectionNavigation}>Work</a>
+        <a href="#motion" onClick={handleSectionNavigation}>Motion</a>
+        <a href="#about" onClick={handleSectionNavigation}>About</a>
+        <a href="#playground" onClick={handleSectionNavigation}>Playground</a>
+        <a href="#contact" onClick={handleSectionNavigation}>Contact</a>
+      </nav>
+
+      <span className="nav-meta">Davao City, PH</span>
+    </header>
+  )
+}
+
+export default Navbar
