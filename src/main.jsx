943268c0ba6fef0b3182d3cbf819a9902af1c5f0
kna <patriciakeanaroma.com@gmail.com>
2026-09-05 11:45:57 +0800

    Load contact balance styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './reference-polish.css'
+import './contact-balance.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
