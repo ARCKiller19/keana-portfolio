d6a9fd2bd29f96748f277243379a56ee779f0c67
kna <patriciakeanaroma.com@gmail.com>
2026-09-10 17:04:04 +0800

    Wire shared booking modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
@@ -5,19 +6,26 @@ import Projects from './components/Projects.jsx'
 import Motion from './components/Motion.jsx'
 import Playground from './components/Playground.jsx'
 import Footer from './components/Footer.jsx'
+import BookingModal from './components/BookingModal.jsx'
 
 function App() {
+  const [isBookingOpen, setIsBookingOpen] = useState(false)
+
+  const openBooking = () => setIsBookingOpen(true)
+  const closeBooking = () => setIsBookingOpen(false)
+
   return (
     <>
-      <Navbar />
+      <Navbar onOpenBooking={openBooking} />
       <main>
-        <Hero />
+        <Hero onOpenBooking={openBooking} />
         <About />
         <Projects />
         <Motion />
         <Playground />
       </main>
       <Footer />
+      <BookingModal isOpen={isBookingOpen} onClose={closeBooking} />
     </>
   )
 }
