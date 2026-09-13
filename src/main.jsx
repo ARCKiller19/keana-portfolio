d92ad5456ecb5506ce4c2c883e0be2a835578a0c
kna <patriciakeanaroma.com@gmail.com>
2026-09-13 10:21:02 +0800

    Load intro navbar guard

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import './interface-cues.css'
 import './motion-signoffs.css'
 import './responsive-guardrails.css'
 import './botanical-motion.css'
+import './intro-navbar-guard.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
